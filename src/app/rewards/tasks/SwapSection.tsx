f968eb3e2e3b7d2f783ce3d48ae185a465a55237
busdcode <ohwovoriole@gmail.com>
2025-05-26 18:00:31 +0100

    updated 40% on token for holders protection

--- src/app/rewards/tasks/SwapSection.tsx
+++ src/app/rewards/tasks/SwapSection.tsx
@@ -115,7 +115,7 @@ const SwapSection = () => {
           ],
         });
         setTxHash(tx);
-        setPendingSwap({ direction: "iSKYOPS_TO_SKYOPS", value: points });
+        setPendingSwap({ direction: "iSKYOPS_TO_SKYOPS", value: points * 0.4 });
       } else {
         // SKYOPS to iSKYOPS: transfer SKYOPS to rewards contract address, then add points after tx confirmed
         const skyopsAmount = Number(amount);
